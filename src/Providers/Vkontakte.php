375c14083d4df72766bc15f9f6512a95e81dbc0c
flarumi <76768930+flarumi@users.noreply.github.com>
2021-01-07 15:15:35 +0300

    Update Vkontakte.php

--- src/Providers/Vkontakte.php
+++ src/Providers/Vkontakte.php
@@ -2,23 +2,14 @@
 
 namespace Flarumi\OauthVkontakte\Providers;
 
+use FoF\OAuth\Provider;
 use Flarum\Forum\Auth\Registration;
 use Flarum\Settings\SettingsRepositoryInterface;
-use FoF\OAuth\Provider;
 use League\OAuth2\Client\Provider\AbstractProvider;
-use J4k\OAuth2\Client\Provider\VkontakteResourceOwner;
 use J4k\OAuth2\Client\Provider\Vkontakte as VkontakteProvider;
 
 class Vkontakte extends Provider
 {
-    protected $settings;
-
-    public function __construct(SettingsRepositoryInterface $settings)
-    {
-        $this->settings = $settings;
-    }
-
-    protected $provider;
 	
     public function name(): string
     {
@@ -42,23 +33,22 @@ class Vkontakte extends Provider
             'client_secret' => 'required',
         ];
     }
+	
    public function provider(string $redirectUri): AbstractProvider
     {
         return $this->provider = new VkontakteProvider([
             'clientId'        => $this->getSetting('client_id'),
             'clientSecret'    => $this->getSetting('client_secret'),
             'redirectUri'     => $redirectUri,
-            'version'         => '5.126',
+            'version'         => '5.126'
         ]);
     }
 
     public function suggestions(Registration $registration, $user, string $token)
     {
-
         $registration
             ->provideTrustedEmail($user->getEmail())
-            ->suggestUsername($user->getName())
-            ->provideAvatar(array_get($user->toArray(), 'photo_100'))
+            ->suggestUsername($user->getScreenName())
             ->setPayload($user->toArray());
     }
 }
