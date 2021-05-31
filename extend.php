97f287bca361e87e422cb58c5adfb938f6388a46
flarumi <76768930+flarumi@users.noreply.github.com>
2021-05-31 20:31:50 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -3,13 +3,11 @@
 namespace Flarumi\OauthVkontakte;
 
 use Flarum\Extend;
-use Flarum\Foundation\Application;
 
 return [
     (new Extend\Frontend('forum'))
         ->css(__DIR__ . '/resources/less/forum.less'),
     new Extend\Locales(__DIR__ . '/resources/locale'),
-    function (Application $app) {
-        $app->register(OauthVkontakteServiceProvider::class);
-    }
+    (new Extend\ServiceProvider())
+        ->register(OauthVkontakteServiceProvider::class),
 ];
